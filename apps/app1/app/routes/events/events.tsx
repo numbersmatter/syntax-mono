a7fbf05f0a8ac0feacb61780804dac3da1682f7c
MarkMutt <lleasy8@gmail.com>
2025-02-10 16:00:29 -0500

    created data table as useable component

--- apps/app1/app/routes/events/events.tsx
+++ apps/app1/app/routes/events/events.tsx
@@ -2,6 +2,10 @@ import { Link, useLoaderData } from "react-router"
 import type { Route } from "./+types/events";
 import { getEvents } from "./events-data.server";
 import { requireAuth } from "~/services/firebase-auth/auth-funcs.server";
+import { Car, ChevronRightIcon, PackageOpenIcon } from "lucide-react";
+import type { EventAppModel } from "~/services/firestore/events/event-types";
+import { DataTable } from "~/components/ui/data-table";
+import type { ColumnDef, SortingState } from "@tanstack/react-table"
 
 
 
@@ -20,14 +24,90 @@ export default function EventsIndex({ loaderData }: Route.ComponentProps) {
   return (
     <div>
       <h1>Events Index</h1>
-      <ul>
-        {events.map((event) => (
-          <li key={event.id}>
-
-            <Link to={`/events/${event.id}`} >{event.name}- {event.id}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventsHistoryTable events={events} />
+      <EventsList events={events} />
     </div>
   )
+}
+
+
+function EventsList({ events }: { events: EventAppModel[] }) {
+  return (
+    <ul
+      role="list"
+      className="divide-y divide-gray-100 overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl"
+    >
+      {events.map((event) => (
+        <li key={event.id} className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6">
+          <div className="flex min-w-0 gap-x-4">
+            {
+              event.type === "pickup" && <Car aria-hidden="true" className="h-5 w-5 flex-none text-gray-400" />
+            }
+            {
+              event.type === "drive-thru" && <PackageOpenIcon aria-hidden="true" className="h-5 w-5 flex-none text-gray-400" />
+            }
+            <div className="min-w-0 flex-auto">
+              <p className="text-sm font-semibold leading-6 text-gray-900">
+                <Link to={`/events/${event.id}`}>
+                  <span className="absolute inset-x-0 -top-px bottom-0" />
+                  {event.name}, {event.type}
+                </Link>
+              </p>
+              <p className="mt-1 flex text-xs leading-5 text-gray-500">
+                {event.eventDate.toLocaleDateString()}
+              </p>
+            </div>
+          </div>
+          <div className="flex shrink-0 items-center gap-x-4">
+            <div className="hidden sm:flex sm:flex-col sm:items-end">
+              <p className="text-sm leading-6 text-gray-900">
+                {event.stage}
+              </p>
+
+            </div>
+            <ChevronRightIcon aria-hidden="true" className="h-5 w-5 flex-none text-gray-400" />
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+
+
+function EventsHistoryTable({ events }: { events: EventAppModel[] }) {
+
+  type EventRow = {
+    id: string;
+    name: string;
+    type: string;
+    eventDate: Date;
+    stage: string;
+  }
+
+  const columns: ColumnDef<EventRow>[] = [
+    {
+      accessorKey: 'name',
+      header: 'Event Name',
+      cell: ({ row }) =>
+        <Link to={`/events/${row.original.id}`}>{row.original.name}</Link>
+    },
+    {
+      accessorKey: 'eventDate',
+      header: 'Event Date',
+      cell: ({ row }) => row.original.eventDate.toLocaleDateString()
+    },
+    {
+      accessorKey: 'id',
+      header: 'Id',
+      cell: ({ row }) => <Link to={`/events/${row.original.id}`}>
+        {row.original.id}
+      </Link>
+    }
+  ]
+
+  const eventSorted = events.sort((a, b) => b.eventDate.valueOf() - a.eventDate.valueOf());
+
+  return <DataTable columns={columns} data={eventSorted} />
+
 }
--- apps/app1/app/services/firestore/events/event-types.ts
+++ apps/app1/app/services/firestore/events/event-types.ts
@@ -1,44 +1,48 @@
-import type { Timestamp } from "firebase-admin/firestore"
+import type { Timestamp } from "firebase-admin/firestore";
 
-export type EventType = "pickup" | "drive-thru" | "error"
-export type EventStage = "planning" | "open-for-requests" | "open-for-pickups" | "event-finished"
+export type EventType = "pickup" | "drive-thru" | "error";
+export type EventStage =
+  | "planning"
+  | "open-for-requests"
+  | "open-for-pickups"
+  | "event-finished";
 
 export interface CreateEvent {
-	name: string
-	type: EventType
-	semesterId: string
-	eventDate: Date
+  name: string;
+  type: EventType;
+  semesterId: string;
+  eventDate: Date;
 }
 export type EventDbModel = {
-	name: string
-	type: EventType
-	semesterId: string
-	eventTimestamp: Timestamp
-	createdTimestamp: Timestamp
-	updatedTimestamp: Timestamp
-	stage: EventStage
-	timeSlots: { [key: number]: string }
-	message: string
-}
+  createdTimestamp: Timestamp;
+  eventTimestamp: Timestamp;
+  message: string;
+  name: string;
+  semesterId: string;
+  stage: EventStage;
+  timeSlots: { [key: number]: string };
+  type: EventType;
+  updatedTimestamp: Timestamp;
+};
 
 export interface EventUpdateableFields {
-	name: string
-	type: EventType
-	stage: EventStage
-	message: string
-	semesterId: string
-	eventDate: Date
+  name: string;
+  type: EventType;
+  stage: EventStage;
+  message: string;
+  semesterId: string;
+  eventDate: Date;
 }
 
 export interface EventAppModel {
-	id: string
-	name: string
-	type: EventType
-	stage: EventStage
-	eventDate: Date
-	createdDate: Date
-	updatedDate: Date
-	timeSlots: { [key: number]: string }
-	semesterId: string
-	message: string
+  id: string;
+  name: string;
+  type: EventType;
+  stage: EventStage;
+  eventDate: Date;
+  createdDate: Date;
+  updatedDate: Date;
+  timeSlots: { [key: number]: string };
+  semesterId: string;
+  message: string;
 }
